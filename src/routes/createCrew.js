4ee1df4a56a9425a6f7910530ff94e3fc5f4e9e3
jjangdol06 <jiyeon971023@gmail.com>
2020-10-27 10:38:21 +0900

    add file upload

--- src/routes/createCrew.js
+++ src/routes/createCrew.js
@@ -2,52 +2,21 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Form, FormGroup, Label, Input, FormText } from 'reactstrap'
 import { TagInput } from 'reactjs-tag-input'
-import { TextField, Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core/';
+import { TextField, Slider, Button } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
 import { ExpandMoreIcon } from '@material-ui/icons/ExpandMore';
 import { Autocomplete } from '@material-ui/lab'
 import Kakaomap from '../component/map/Kakaomap'
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: '100%',
-//   },
-//   heading: {
-//     fontSize: theme.typography.pxToRem(15),
-//   },
-//   secondaryHeading: {
-//     fontSize: theme.typography.pxToRem(15),
-//     color: theme.palette.text.secondary,
-//   },
-//   icon: {
-//     verticalAlign: 'bottom',
-//     height: 20,
-//     width: 20,
-//   },
-//   details: {
-//     alignItems: 'center',
-//   },
-//   column: {
-//     flexBasis: '33.33%',
-//   },
-//   helper: {
-//     borderLeft: `2px solid ${theme.palette.divider}`,
-//     padding: theme.spacing(1, 2),
-//   },
-//   link: {
-//     color: theme.palette.primary.main,
-//     textDecoration: 'none',
-//     '&:hover': {
-//       textDecoration: 'underline',
-//     },
-//   },
-// }))
-
+// add province and gender
 class Createcrew extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { tags: [], value: new Date(), onChange: new Date(), ex: [] }
+    this.state = {
+      tags: [], value: new Date(), onChange: new Date(), ex: [],
+      province: ["서울", "경기", "인천", "대전", "부산", "대구", "광주", "전주", "울산", "나주"]
+    }
     this.onTagsChanged = this.onTagsChanged.bind(this);
     // const [value, onChange] = useState([new Date(), new Date()]);
   }
@@ -71,14 +40,14 @@ class Createcrew extends Component {
   }
 
   render() {
-    const { tags, value, onChange, ex } = this.state
+    const { tags, value, onChange, ex, province } = this.state
     const classes = this.props;
     // const classes = useStyles();
 
     return (
-      <div>
-        <Form>
-          <form className={classes.container} noValidate>  
+      <div style={{ width: "70%", margin: "auto" }}>
+        <Form style={{display:"grid"}}>
+          <form className={classes.container} noValidate>
             <TextField
               required
               id="outlined-required"
@@ -86,27 +55,28 @@ class Createcrew extends Component {
               placeholder="크루 이름을 입력해주세요"
               variant="outlined"
             />
-            {/* <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography className={classes.heading}>위치 선택하기</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <div>
-                  <Kakaomap />
-                </div>
-              </AccordionDetails>
-            </Accordion> */}
-            {/* 태그형식으로 autocomplete, add tag */}
+            <input type="file" name="FileName" multiple />
             <Autocomplete
               id="combo-box-demo"
               options={ex}
               getOptionLabel={(option) => option.type}
               style={{ width: 300 }}
               renderInput={(params) => <TextField {...params} label="운동 종목" variant="outlined" />}
+            />
+            <Input type="select" name="select" id="exampleSelect">
+              <option>여성</option>
+              <option>남성</option>
+              <option>혼성</option>
+            </Input>
+            <Input type="select" name="select" id="exampleSelect">
+              {province.map((data, i) => <option id={i}>{data}</option>)}
+            </Input>
+            <TextField
+              required
+              id="outlined-required"
+              label="장소"
+              placeholder="장소를 입력해주세요"
+              variant="outlined"
             />
             <TextField
               id="datetime-local"
@@ -147,7 +117,38 @@ class Createcrew extends Component {
             placeholder="크루 상세 설명을 적어주세요"
             variant="outlined"
           />
-          <TagInput tags={tags} onTagsChanged={this.onTagsChanged} />
+          <TextField
+            id="outlined-multiline-static"
+            label="크루장 소개"
+            multiline
+            rows={6}
+            placeholder="크루 상세 설명을 적어주세요"
+            variant="outlined"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="준비물"
+            placeholder="준비물을 입력해주세요"
+            variant="outlined"
+          />
+          <Slider
+            defaultValue={20}
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="auto"
+            step={10}
+            marks
+            min={10}
+            max={60}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="해쉬태그"
+            placeholder="해쉬태그 입력해주세요"
+            variant="outlined"
+          />
+          <Button>생성하기</Button>
         </Form>
       </div>
     )
